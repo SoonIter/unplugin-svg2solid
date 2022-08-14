b1295d10b7afedbd2019cc09720a5d71cd3de8e1
YeanSe <1730214056@qq.com>
2022-08-15 03:14:13 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
-import { createUnplugin } from 'unplugin'
-import type { Options } from './types'
+import { readFileSync } from 'fs';
+import { createUnplugin } from 'unplugin';
+import type { Options } from './types';
+import fs from 'fs';
+import resolve from 'resolve';
+import { SolidCompiler } from './utils/solidCompCompiler';
+import path from 'path';
 
-export default createUnplugin<Options>(options => ({
-  name: 'unplugin-starter',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
-}))
+export default createUnplugin<Options>(options => {
+  // let resolveId = '.svg';
+  // let _code = '';
+  const m = new Map<string, string>();
+
+  return {
+    name: 'unplugin-svg2solidcomp',
+    enforce: 'pre',
+
+    resolveId(id, importee) {
+      if (id.endsWith('.svg')) {
+        const where = path.resolve(path.dirname(importee as string), id);
+        const code = fs.readFileSync(where, 'utf-8');
+        const resolveId = where.replace('.svg', '.tsx');
+        m.set(resolveId, code);
+        return resolveId;
+      }
+    },
+    async load(id: string) {
+      const key = [...m.keys()].find(i => i.includes(id));
+
+      if (key) {
+        return { code: SolidCompiler(m.get(key) ?? '') ?? '' };
+      }
+    },
+  };
+});
